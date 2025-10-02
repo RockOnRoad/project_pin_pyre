8c9c3e2dcb1e6535fbcfa698895a5a08365167dd
RockOnRoad <iHushHost@gmail.com>
2025-10-02 18:31:55 +0300

    added users table

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,5 +1,12 @@
-__all__ = ("Base", "DatabaseUtility", "db_util", "Product")
+__all__ = (
+    "Base",
+    "DatabaseUtility",
+    "db_util",
+    "Product",
+    "User",
+)
 
 from .base import Base
 from .db_util import DatabaseUtility, db_util
 from .product import Product
+from .user import User
